a3854e22e49c31addfcbfb55c5d487c33dc24cfa
PaoloPublicista <paolopublicista@gmail.com>
2024-02-16 14:35:50 -0500

    primer commit de los archivos

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import Perfil from './componentes/Perfil';
+
+function App() {
+  return (
+    <div className="App">
+     <div class ='contenedor-principal'>
+      <h1>Mis YouTubers Favoritos</h1>
+      <Perfil 
+        nombre='Brais Moure'
+        pais='España'
+        imagen='brais'
+        cargo='Ingeniero de Software'
+        empresa='MoureDev'
+        perfil='Ingeniero de software desde 2010. Actualmente trabaja como desarrollador freelance full-stack. Además, crea contenido formativo y divulgo sobre programación y desarrollo de software en redes como @mouredev. Fundador de la empresa Moure.dev'
+      />
+      <Perfil 
+        nombre='Romuald Fons'
+        pais='España'
+        imagen='romuald'
+        cargo='SEO & Marketer'
+        empresa='Big SEO'
+        perfil='Fundador del holding BIGSEO, Best 100 Influencer (Forbes 2021), Top Business Influencer (Eshow 2022) y reconocido como uno de los marketers más influyentes a nivel internacional.
+
+        Hace más de 10 años que Romuald Fons ayuda a empresas y emprendedores a mejorar sus negocios con marketing digital, impulsando la mayor comunidad de SEO de habla hispana del mundo y formando a más de 1 millón de personas en sus redes sociales.'
+      />
+      <Perfil 
+        nombre='Nicolás Schürmann Lindemann'
+        pais='Chile'
+        imagen='nicolas'
+        cargo='Ingeniero de Software'
+        empresa='Hola Mundo'
+        perfil='Es un Youtuber Chileno, ingeniero de software desde hace 8 años, trabajar con PHP nodeJS, react y react native. Es el fundador de la empresa de desarrollo de software Hola Mundo'
+      />
+     </div>
+    </div>
+  );
+}
+
+export default App;
